56c2e0bf76b843b7b1a0d1ab0bcb82b75e35dd12
Bielorusse <thibautvoirand@gmail.com>
2021-07-07 21:30:56 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# s2coastalbot
+
+Twitter bot that posts newly acquired Sentinel-2 images of coastal areas.
+
+Heavily inspired from sentinel2-bot.
---
+++ s2coastalbot.py
@@ -0,0 +1,64 @@
+"""
+s2coastalbot main script.
+Twitter bot that posts newly acquired Sentinel-2 images of coastal areas.
+"""
+
+# standard imports
+import os
+import argparse
+import configparser
+
+# third party imports
+import tweepy
+
+
+class S2CoastalBot():
+    """
+    Class for S2CoastalBot.
+    """
+
+    def __init__(self):
+        """
+        Constructor for the S2CoastalBot class.
+        """
+
+        print("not implemented yet")
+
+    def authenticate():
+        """
+        Twitter authentication.
+        """
+
+        # read config file
+        project_path = os.path.dirname(os.path.realpath(__file__))
+        config = configparser.ConfigParser()
+        config.read(os.path.join(project_path, "config", "config.ini"))
+        consumer_key = config.get("access", "consumer_key")
+        consumer_secret = config.get("access", "consumer_secret")
+        access_token = config.get("access", "access_token")
+        access_token_secret = config.get("access", "access_token_secret")
+
+        # twitter authentification
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+
+    def post_tweet():
+        """
+        Post tweet of newly acquired Sentinel-2 coastal image.
+        """
+
+        api.update_status(status="Not implemented yet")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+    args = parser.parse_args()
+
+    s2coastalbot = S2CoastalBot()
+
+    s2coastalbot.authenticate()
+
+    s2coastalbot.post_tweet()
