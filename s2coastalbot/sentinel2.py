bb734b660cbf71f7fbb92c588ec97dd475080d96
Bielorusse <thibautvoirand@gmail.com>
2022-02-26 10:05:01 +0100

    Increase retries and move inner functions in download module

--- s2coastalbot/sentinel2.py
+++ s2coastalbot/sentinel2.py
@@ -4,6 +4,7 @@ Sentinel-2 data handling module for s2coastalbot.
 
 # standard imports
 import os
+import sys
 import argparse
 import configparser
 import json
@@ -28,6 +29,92 @@ from s2coastalbot.geoutils import get_location_name
 from s2coastalbot.custom_logger import get_custom_logger
 
 
+def find_tci_file(product_path):
+    """Look for TCI file within S2 L2A product."""
+    for path, dirs, files in os.walk(product_path):
+        for f in files:
+            if f.lower().endswith("_tci_10m.jp2"):
+                return os.path.join(path, f)
+    return None
+
+
+def find_mtd_file(product_path):
+    """Look for MTD file within S2 L2A product."""
+    for path, dirs, files in os.walk(product_path):
+        for f in files:
+            if f.lower().endswith("mtd_msil2a.xml"):
+                return os.path.join(path, f)
+    return None
+
+
+def read_nodata_pixel_percentage(mtd_file):
+    """Read nodata pixel percentage from L2A product metadata file."""
+    tree = ET.parse(mtd_file)
+    root = tree.getroot()
+    quality_indicators_info = root.find(
+        "{https://psd-14.sentinel2.eo.esa.int/PSD/User_Product_Level-2A.xsd}Quality_Indicators_Info"
+    )
+    image_content_qi = quality_indicators_info.find("Image_Content_QI")
+    nodata_pixel_percentage = float(image_content_qi.find("NODATA_PIXEL_PERCENTAGE").text)
+    return nodata_pixel_percentage
+
+
+def read_nodata_from_l2a_prod(product_series, output_folder, products_api, logger):
+    """Read nodata pixels percentage in L2A product."""
+
+    # download L2A product metadata file
+    nodefilter = make_path_filter("*mtd_msil2a.xml")
+    sentinelsat_retry_download(
+        products_api,
+        product_series["uuid"],
+        output_folder,
+        nodefilter,
+        logger,
+    )
+
+    # read metadata file to check nodata pixels percentage
+    l2a_safe_path = os.path.join(output_folder, product_series["filename"])
+    l2a_mtd_file = find_mtd_file(l2a_safe_path)
+    return read_nodata_pixel_percentage(l2a_mtd_file)
+
+
+def sentinelsat_retry_download(api, uuid, output_folder, nodefilter, logger):
+    """Download product with sentinelsat api with retry and backoff."""
+
+    # initiate sleep time
+    sleep_time = 2
+
+    # limit to 5 tries
+    for retry_nb in range(10):
+
+        try:
+            product_info = api.download(uuid, directory_path=output_folder, nodefilter=nodefilter)
+            error_str = None
+
+        except Exception as error:
+            error_str = error
+            logger.error(
+                "Failed sentinelsat download after {} retries, waiting {} seconds".format(
+                    retry_nb, sleep_time
+                )
+            )
+            logger.error(error_str)
+            pass
+
+        # wait for some given time and increase time at each retry
+        if error_str:
+            sleep(sleep_time)
+            sleep_time *= 2
+
+        # leave retry loop in case of success
+        else:
+            return product_info
+
+    # report failure after too many retries
+    logger.error("Failed sentinelsat download stopped processing")
+    sys.exit(128)
+
+
 def download_tci_image(
     copernicus_user, copernicus_password, aoi_file, output_folder=None, logger=None
 ):
@@ -43,85 +130,6 @@ def download_tci_image(
         -tci_file_path          str
         -                       datetime.datetime
     """
-
-    def find_tci_file(product_path):
-        """Look for TCI file within S2 L2A product."""
-        for path, dirs, files in os.walk(product_path):
-            for f in files:
-                if f.lower().endswith("_tci_10m.jp2"):
-                    return os.path.join(path, f)
-        return None
-
-    def find_mtd_file(product_path):
-        """Look for MTD file within S2 L2A product."""
-        for path, dirs, files in os.walk(product_path):
-            for f in files:
-                if f.lower().endswith("mtd_msil2a.xml"):
-                    return os.path.join(path, f)
-        return None
-
-    def read_nodata_pixel_percentage(mtd_file):
-        """Read nodata pixel percentage from L2A product metadata file."""
-        tree = ET.parse(mtd_file)
-        root = tree.getroot()
-        quality_indicators_info = root.find(
-            "{https://psd-14.sentinel2.eo.esa.int/PSD/User_Product_Level-2A.xsd}Quality_Indicators_Info"
-        )
-        image_content_qi = quality_indicators_info.find("Image_Content_QI")
-        nodata_pixel_percentage = float(image_content_qi.find("NODATA_PIXEL_PERCENTAGE").text)
-        return nodata_pixel_percentage
-
-    def read_nodata_from_l2a_prod(product_series, output_folder, products_api, logger):
-        """Read nodata pixels percentage in L2A product."""
-
-        # download L2A product metadata file
-        nodefilter = make_path_filter("*mtd_msil2a.xml")
-        sentinelsat_retry_download(
-            products_api,
-            product_series["uuid"],
-            output_folder,
-            nodefilter,
-            logger,
-        )
-
-        # read metadata file to check nodata pixels percentage
-        l2a_safe_path = os.path.join(output_folder, product_series["filename"])
-        l2a_mtd_file = find_mtd_file(l2a_safe_path)
-        return read_nodata_pixel_percentage(l2a_mtd_file)
-
-    def sentinelsat_retry_download(api, uuid, output_folder, nodefilter, logger):
-        """Download product with sentinelsat api with retry and backoff."""
-
-        # initiate sleep time
-        sleep_time = 2
-
-        # limit to 5 tries
-        for retry_nb in range(5):
-
-            try:
-                product_info = api.download(
-                    uuid, directory_path=output_folder, nodefilter=nodefilter
-                )
-                error_str = None
-
-            except Exception as error:
-                error_str = error
-                logger.error(
-                    "Failed sentinelsat download after {} retries, waiting {} seconds".format(
-                        retry_nb, sleep_time
-                    )
-                )
-                logger.error(error_str)
-                pass
-
-            # wait for some given time and increase time at each retry
-            if error_str:
-                sleep(sleep_time)
-                sleep_time *= 2
-
-            # leave retry loop in case of success
-            else:
-                return product_info
 
     # create logger if necessary
     if logger is None:
