e0972eb64f5d5d2c00cd48f11f09c9ad5d5caaa5
Bielorusse <thibautvoirand@gmail.com>
2022-07-29 14:53:34 +0200

    Bump version

--- s2coastalbot/custom_logger.py
+++ s2coastalbot/custom_logger.py
@@ -9,7 +9,7 @@ from logging.handlers import RotatingFileHandler
 import socket
 
 APP_NAME = "s2coastalbot"
-APP_VERSION = "0.4.1"
+APP_VERSION = "0.5"
 
 
 def get_custom_logger(log_file, level=20):
