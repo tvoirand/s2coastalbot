e59d8fc4a37fd251020f37b6158e89ddfec27726
Bielorusse <thibautvoirand@gmail.com>
2022-09-25 18:05:35 +0200

    Improve logs reading script with time spent on each process

--- s2coastalbot/read_logs.py
+++ s2coastalbot/read_logs.py
@@ -22,13 +22,15 @@ def read_logs(log_file):
     """
 
     # initiate logs dataframe
-    logs = pd.DataFrame(columns=["date", "pid", "level", "message"])
+    logs = pd.DataFrame(columns=["date", "pid", "time_spent", "level", "message"])
 
     # read log file
     with open(log_file, "r") as infile:
 
         # initiate some loop variables
-        current_pid = infile.readline().split()[4][1:-2]
+        first_line = infile.readline()
+        start_time = datetime.datetime.strptime(first_line.split()[0], "%Y-%m-%dT%H:%M:%S.%f")
+        current_pid = first_line.split()[4][1:-2]
         message = ""
 
         for line in infile.readlines():
@@ -42,13 +44,16 @@ def read_logs(log_file):
             if (
                 pid != current_pid
             ):  # in case of new process, store infos from previous line
+                end_time = datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
                 logs.loc[len(logs)] = [
-                    datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f"),
+                    end_time,
                     current_pid,
+                    (end_time - start_time).seconds / 60,
                     level,
                     message,
                 ]
                 current_pid = pid
+                start_time = datetime.datetime.strptime(line.split()[0], "%Y-%m-%dT%H:%M:%S.%f")
 
             date_str = line.split()[0]
             level = line.split()[5][1:-1]
